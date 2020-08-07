a3eec846a7631edb840302fbe3308299c932e821
Raziel <3615018+rraziel@users.noreply.github.com>
2020-08-08 00:26:11 +0200

    Fixed 404.js

--- pages/404.js
+++ pages/404.js
@@ -3,7 +3,7 @@ import Error from '../components/Error';
 const Error404Component = () => {
   const statusCode = 404;
 
-  <Error statusCode={statusCode} />;
+  return <Error statusCode={statusCode} />;
 };
 
 export default Error404Component;
