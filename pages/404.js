eb58d7a109cfd54da83e0ed328572bb2d6f48530
Raziel <3615018+rraziel@users.noreply.github.com>
2020-08-08 00:23:23 +0200

    Fixed error/404 page after version upgrade

---
+++ pages/404.js
@@ -0,0 +1,9 @@
+import Error from '../components/Error';
+
+const Error404Component = () => {
+  const statusCode = 404;
+
+  <Error statusCode={statusCode} />;
+};
+
+export default Error404Component;
